de6359dca9e96086abb78122dff6a04868674c33
Kaushal <kaushalshah868@gmail.com>
2018-05-09 11:21:24 +0530

    Initial commit

---
+++ dbmanager/src/main/java/com/cpc/dbmanager/DbmanagerApplication.java
@@ -0,0 +1,12 @@
+package com.cpc.dbmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmanagerApplication.class, args);
+	}
+}
